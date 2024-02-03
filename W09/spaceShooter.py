0e4bb4db43df2a0ade3de35fcc872721cf483d80
Mingguriguri <rlaalswjd-@naver.com>
2024-02-03 14:30:06 +0900

    [W09] 수업시간

--- W09/spaceShooter.py
+++ W09/spaceShooter.py
@@ -8,6 +8,10 @@ height = 500
 screen = pygame.display.set_mode((width, height))
 
 # 배경
+bgImage = pygame.image.load('test.png')
+bgImage = pygame.transform.scale(bgImage, (width, height))
+backX = 0 # 좌표 A의 초기값
+backX2 = width # 좌표 B의 초기값
 
 while True:
 	for event in pygame.event.get():
@@ -15,5 +19,7 @@ while True:
 			pygame.quit()
 			sys.exit()
 
+	screen.blit(bgImage, (backX, 0))
+	screen.blit(bgImage, (backX2, 0))
 
 	pygame.display.update()
